de1b3652715623bdc3dcd20c2e0547470ccba63d
Andy Kayley <bitbucket@kayley.name>
2020-03-08 20:21:44 +0000

    initial commit

---
+++ src/modules/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
+import counter from './counter'
+
+const createRootReducer = (history) => combineReducers({
+    router: connectRouter(history),
+    counter
+})
+
+export default createRootReducer
