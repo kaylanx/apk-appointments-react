5bb0d28e314089ce35a564d969976b7f5f4e68f2
Andy Kayley <bitbucket@kayley.name>
2020-03-11 23:57:24 +0000

    WIP trying to get disabling of dates working with isTomorrow etc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { TextField } from '@material-ui/core';
+import DateFnsUtils from '@date-io/date-fns';
+import isTomorrow from '@date-io/date-fns';
+import {
+  DatePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
+
+const disabledDates = (date) => {
+  console.log(date)
+  console.log(typeof date)
+  console.log(Object.prototype.toString.call(date))
+  //return date.getDay() === 0 || date.getDay() === 6;
+
+  return isTomorrow(date)
+}
 
 function App() {
+  
+  const [selectedDate, handleDateChange] = useState(new Date("2020-03-12"));
+
+  const classes = useStyles();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form className={classes.root} noValidate autoComplete="off">
+
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <DatePicker
+              label="Preferred Date"
+              required
+              value={selectedDate}
+              onChange={handleDateChange}
+              disablePast={true}
+              autoOk={true}
+              minDate={new Date("2020-03-12")}
+              shouldDisableDate={disabledDates}
+              inputVariant="filled" />
+          </MuiPickersUtilsProvider>
+          <TextField
+            id="time"
+            label="Preferred Time"
+            required
+            type="time"
+            variant="filled"
+            defaultValue="07:30"
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            inputProps={{
+              step: 300, // 5 min
+            }}
+          />
+          <TextField id="filled-basic" label="Filled" variant="filled" />
+
+          <TextField id="standard-basic" label="Standard" />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </form>
     </div>
   );
 }
 
 export default App;
+
+/*
+
+Preferred Time (required)
+
+
+Appointment Type (required)
+
+
+
+How many bridesmaids do you have?
+
+
+Wedding / Event Date
+
+
+dd/mm/yyyy
+
+Your Name (required)
+
+
+Your Email (required)
+
+
+Confirm Email (required)
+
+
+Your phone number (required)
+
+
+Budget
+
+
+How did you hear about us?
+
+
+Your Message
+*/
