ad82f6689f2511b0221c627991c9387bfc447c36
Andy Kayley <bitbucket@kayley.name>
2020-04-04 17:39:08 +0100

    refactored the appointment-time.test.js

--- src/appointment-time.test.js
+++ src/appointment-time.test.js
@@ -22,78 +22,55 @@ describe('appointments time', () => {
     await act(async () => {
       render(<AppointmentTime id="appointment-time" selectedDate={thursdayNinthApril} diary={actualSchedule} />, container)
     })
-
-    const input = document.querySelector('[id=appointment-time]')
-    clickInput(input)
-    const times = document.querySelectorAll('option')
-    expect(times).not.toBeUndefined()
-    expect(times).toHaveLength(1)
-
-    let index = 0
-    expect(times[index++].textContent).toBe('')
+    await expectNoOptions(thursdayNinthApril, actualSchedule)
   })
 
   it('list to contain no options when data is broken', async () => {
     const tuesdaySeventhApril = new Date('2020-04-07')
-    await act(async () => {
-      render(<AppointmentTime id="appointment-time" selectedDate={tuesdaySeventhApril} diary={{}} />, container)
-    })
+    const diary = {}
+    await expectNoOptions(tuesdaySeventhApril, diary)
+  })
 
+  it('list to contain 12 hour format from 10am to 4pm', async () => {
+    const mondayMarch30th2020 = new Date('2020-03-30')
+    const diary = actualSchedule
+    const expectedTimes = ['', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm']
+    await assertExpectedTimesForDateAndSchedule(mondayMarch30th2020, diary, expectedTimes)
+  })
+
+  it('list to contain 24 hour format from 10:00 to 16:00', async () => {
+    const mondayMarch30th2020 = new Date('2020-03-30')
+    const diary = actualSchedule24Hours
+    const expectedTimes = ['', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00']
+    await assertExpectedTimesForDateAndSchedule(mondayMarch30th2020, diary, expectedTimes)
+  })
+
+  async function expectNoOptions (selectedDate, diary) {
+    await act(async () => {
+      render(<AppointmentTime id="appointment-time" selectedDate={selectedDate} diary={diary} />, container)
+    })
     const input = document.querySelector('[id=appointment-time]')
     clickInput(input)
     const times = document.querySelectorAll('option')
     expect(times).not.toBeUndefined()
     expect(times).toHaveLength(1)
-
     let index = 0
     expect(times[index++].textContent).toBe('')
-  })
+  }
 
-  it('list to contain 12 hour format from 10am to 4pm', async () => {
-    const mondayMarch30th2020 = new Date('2020-03-30')
+  async function assertExpectedTimesForDateAndSchedule (mondayMarch30th2020, diary, expectedTimes) {
     await act(async () => {
-      render(<AppointmentTime id="appointment-time" selectedDate={mondayMarch30th2020} diary={actualSchedule} />, container)
+      render(<AppointmentTime id="appointment-time" selectedDate={mondayMarch30th2020} diary={diary} />, container)
     })
-
     const input = document.querySelector('[id=appointment-time]')
     clickInput(input)
     const times = document.querySelectorAll('option')
     expect(times).not.toBeUndefined()
     expect(times).toHaveLength(8)
 
-    let index = 0
-    expect(times[index++].textContent).toBe('')
-    expect(times[index++].textContent).toBe('10:00am')
-    expect(times[index++].textContent).toBe('11:00am')
-    expect(times[index++].textContent).toBe('12:00pm')
-    expect(times[index++].textContent).toBe('1:00pm')
-    expect(times[index++].textContent).toBe('2:00pm')
-    expect(times[index++].textContent).toBe('3:00pm')
-    expect(times[index++].textContent).toBe('4:00pm')
-  })
-
-  it('list to contain 24 hour format from 10:00 to 16:00', async () => {
-    const mondayMarch30th2020 = new Date('2020-03-30')
-    await act(async () => {
-      render(<AppointmentTime id="appointment-time" selectedDate={mondayMarch30th2020} diary={actualSchedule24Hours} />, container)
-    })
-
-    const input = document.querySelector('[id=appointment-time]')
-    clickInput(input)
-    const times = document.querySelectorAll('option')
-    expect(times).not.toBeUndefined()
-    expect(times).toHaveLength(8)
-
-    let index = 0
-    expect(times[index++].textContent).toBe('')
-    expect(times[index++].textContent).toBe('10:00')
-    expect(times[index++].textContent).toBe('11:00')
-    expect(times[index++].textContent).toBe('12:00')
-    expect(times[index++].textContent).toBe('13:00')
-    expect(times[index++].textContent).toBe('14:00')
-    expect(times[index++].textContent).toBe('15:00')
-    expect(times[index++].textContent).toBe('16:00')
-  })
+    const actualTimes = Array.from(times).map((time) => time.textContent)
+    expect(actualTimes).toEqual(expect.arrayContaining(expectedTimes))
+  }
 })
 
 const clickInput = (input) => {
