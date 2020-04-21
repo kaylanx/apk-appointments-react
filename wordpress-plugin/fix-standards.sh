7535ae185c84defb7cafc0f5d5eacf643d53951d
Andy Kayley <bitbucket@kayley.name>
2020-04-21 23:00:54 +0100

    Fix wordpress standards in rest endpoint file

---
+++ wordpress-plugin/fix-standards.sh
@@ -0,0 +1,1 @@
+./vendor/bin/phpcbf --standard=wordpress *.php includes/*.php
--- wordpress-plugin/includes/apk-appointments-enable-rest-endpoint.php
+++ wordpress-plugin/includes/apk-appointments-enable-rest-endpoint.php
@@ -1,13 +1,19 @@
-
 <?php
 function get_appointments( $data ) {
 	$appointments = get_option( 'apk_appointments_options' );
 	return $appointments;
 }
 
-add_action( 'rest_api_init', function () {
-  register_rest_route( 'apk-appointments/v1', '/appointments', array(
-    'methods' => 'GET',
-    'callback' => 'get_appointments',
-  ) );
-} );
+add_action(
+	'rest_api_init',
+	function () {
+		register_rest_route(
+			'apk-appointments/v1',
+			'/appointments',
+			array(
+				'methods'  => 'GET',
+				'callback' => 'get_appointments',
+			)
+		);
+	}
+);
