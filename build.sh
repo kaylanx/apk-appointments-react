ed3b81174a392c6a5da741fdb63a67640664be15
Andy Kayley <bitbucket@kayley.name>
2020-04-21 23:46:38 +0100

    Creating build for the wordpress plugin

---
+++ build.sh
@@ -0,0 +1,4 @@
+mkdir -p build/apk-appointments
+cp -r wordpress-plugin/includes build/apk-appointments
+cp wordpress-plugin/*.php build/apk-appointments
+cp wordpress-plugin/README.txt build/apk-appointments
