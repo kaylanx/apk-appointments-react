6e344ea585e5c15087851514f756a87702aa02a8
Andy Kayley <bitbucket@kayley.name>
2020-04-22 08:32:01 +0100

    Build now includes react-app in the wordpress plugin

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 mkdir -p build/apk-appointments
 cp -r wordpress-plugin/includes build/apk-appointments
 cp wordpress-plugin/*.php build/apk-appointments
-cp wordpress-plugin/README.txt build/apk-appointments
+cp wordpress-plugin/readme.txt build/apk-appointments
+
+cd react-app
+npm run build
+cd ..
+cp -r react-app/build/static/ build/apk-appointments/includes/
+ 
