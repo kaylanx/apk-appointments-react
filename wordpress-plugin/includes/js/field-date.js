2702fe75dbaa81ee688c17a396caccf5c7d3888d
Andy Kayley <bitbucket@kayley.name>
2020-04-22 08:24:16 +0100

    Remove the old jQuery implementation

--- wordpress-plugin/includes/js/field-date.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery('.apk-appointment-date').datepicker({ 
-		dateFormat: 'yy-mm-dd',
-		numberOfMonths: 3,
-      	showButtonPanel: true,
-        changeMonth: true,
-        changeYear: true 
-	});	
-});
