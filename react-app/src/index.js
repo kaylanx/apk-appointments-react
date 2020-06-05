54863eb6494b818ba339b21e4996dc4991179e10
Andy Kayley <bitbucket@kayley.name>
2020-06-05 08:37:07 +0100

    pass in the firebase config - not happy with this solution - need to rethink

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -6,20 +6,9 @@ import './index.css'
 
 const target = document.querySelector('#apk-appointments')
 
-var firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-}
-
 render(
   <div>
-    <App analyticsConfig={firebaseConfig} />
+    <App analyticsConfig={window.apkAppointmentsFirebaseConfig} />
   </div>,
   target
 )
